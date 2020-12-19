a610d64cbf7ce440ef345cf0fb9e598b037530a2
Malique_Auguste <maliqueauguste@gmail.com>
2020-12-18 22:42:31 -0400

    Finished Fizz Buzz
    Duplicate opcode (DUP) was created 2 commits ago.

--- examples/fizz_buzz.rs
+++ examples/fizz_buzz.rs
@@ -10,32 +10,36 @@ fn main() {
         Opcode::encode(Opcode::PUSH, 1, 0),
         Opcode::encode(Opcode::ADD, 0, 0),
 
-        //Checks if not divisible by 15
-        Opcode::encode(Opcode::DUP, 0, 0),
-        Opcode::encode(Opcode::PUSH, 15, 0),
-        Opcode::encode(Opcode::MOD, 0, 0),
-        Opcode::encode(Opcode::JE, 4, 1),
-
-        Opcode::encode(Opcode::POP, 0, 0),
-        Opcode::encode(Opcode::STDOUT, 0, 0),
-
         //Checks if it is divisible by 3
         Opcode::encode(Opcode::DUP, 0, 0),
         Opcode::encode(Opcode::PUSH, 3, 0),
         Opcode::encode(Opcode::MOD, 0, 0),
-        Opcode::encode(Opcode::JNE, 4, 1),
-
-        //prints f
-        Opcode::encode(Opcode::PUSH, 102, 0),        //46 is the utf8 code for f
-        Opcode::encode(Opcode::STDOUT, 0, 1),
-        Opcode::encode(Opcode::POP, 0, 0),
+        Opcode::encode(Opcode::JE, 15, 1),
 
         //Checks if it is divisible by 5
         Opcode::encode(Opcode::POP, 0, 0),
         Opcode::encode(Opcode::DUP, 0, 0),
         Opcode::encode(Opcode::PUSH, 5, 0),
         Opcode::encode(Opcode::MOD, 0, 0),
-        Opcode::encode(Opcode::JNE, 4, 1),
+        Opcode::encode(Opcode::JE, 14, 1),
+
+        Opcode::encode(Opcode::POP, 0, 0),
+        Opcode::encode(Opcode::STDOUT, 0, 0),
+        Opcode::encode(Opcode::JMP, 15, 1),
+
+        //Checks if it is divisible by 5
+        Opcode::encode(Opcode::POP, 0, 0),
+        Opcode::encode(Opcode::DUP, 0, 0),
+        Opcode::encode(Opcode::PUSH, 5, 0),
+        Opcode::encode(Opcode::MOD, 0, 0),
+        Opcode::encode(Opcode::JE, 6, 1),
+        Opcode::encode(Opcode::JNE, 8, 1),
+
+        //prints f
+        Opcode::encode(Opcode::PUSH, 102, 0),        //46 is the utf8 code for f
+        Opcode::encode(Opcode::STDOUT, 0, 1),
+        Opcode::encode(Opcode::POP, 0, 0),
+        Opcode::encode(Opcode::JMP, 15, 0),
 
         //prints b
         Opcode::encode(Opcode::PUSH, 98, 0),        //98 is the utf8 code for b
